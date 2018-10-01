e16909c172c348e5990a903583c50c142bea94b6
Nick <grifon52@gmail.com>
2018-10-01 03:09:37 +0300

    Add names to relationships

--- processor/cache/nifi-processor-cache/src/main/java/com/github/gr1f0n6x/processor/cache/utils/Relationships.java
+++ processor/cache/nifi-processor-cache/src/main/java/com/github/gr1f0n6x/processor/cache/utils/Relationships.java
@@ -4,14 +4,18 @@ import org.apache.nifi.processor.Relationship;
 
 public class Relationships {
     public static final Relationship SUCCESS = new Relationship.Builder()
+            .name("success")
             .build();
 
     public static final Relationship FAILURE = new Relationship.Builder()
+            .name("failure")
             .build();
 
     public static final Relationship EXIST = new Relationship.Builder()
+            .name("exist")
             .build();
 
     public static final Relationship NOT_EXIST = new Relationship.Builder()
+            .name("not exist")
             .build();
 }
